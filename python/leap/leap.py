75d16565d9582ece000900022ff88549636f5d8e
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-08 17:33:37 -0500

    Passing

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,12 @@
 def leap_year(year):
-    pass
+    """
+    returns if a year is a leap year
+    :param year: year in question
+    :return: boolean - true if leap year
+    """
+
+    divisible_by_4 = year % 4 == 0
+    divisible_by_100 = year % 100 == 0
+    divisible_by_400 = year % 400 == 0
+
+    return divisible_by_4 and (not divisible_by_100 or divisible_by_400)
