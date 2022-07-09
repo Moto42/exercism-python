34edf9bab82e553826eb8bfcc5ed8267f45358f8
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-08 21:29:21 -0500

    Passing

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,19 @@
 def response(hey_bob):
-    pass
+
+    hoi_bob = hey_bob.strip()
+
+    is_question = hoi_bob.endswith("?")
+    is_yelled = hoi_bob.isupper()
+    is_empty = len(hoi_bob) == 0
+
+    if is_empty:
+        bob = "Fine. Be that way!"
+    elif is_yelled and is_question:
+        bob = "Calm down, I know what I'm doing!"
+    elif is_yelled and not is_question:
+        bob = "Whoa, chill out!"
+    elif is_question:
+        bob = "Sure."
+    else:
+        bob = "Whatever."
+    return bob
