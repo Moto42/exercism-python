37b4e84e871ce5f5e3bb9a32e4836aa0f3f74634
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 18:37:46 -0500

    exercism download --exercise=difference-of-squares --track=python

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,22 @@
+import functools
+
+
+def summer(acc, cur):
+    return acc + cur
+
+
+def squarm(acc, cur):
+    return acc + cur ** 2
+
+
 def square_of_sum(number):
-    pass
+    sum_num = functools.reduce(summer,range(1,number + 1), 0)
+    return sum_num ** 2
 
 
 def sum_of_squares(number):
-    pass
+    return functools.reduce(squarm, range(1, number + 1), 0)
 
 
 def difference_of_squares(number):
-    pass
+    return abs(sum_of_squares(number) - square_of_sum(number))
