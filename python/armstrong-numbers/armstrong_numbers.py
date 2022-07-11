ea171cb012734c1b4e863247448e1a466c6ac685
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 18:26:27 -0500

    passing

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,12 @@
+import functools
+
+
 def is_armstrong_number(number):
-    pass
+    digits = [int(char) for char in str(number)]
+    num_digits = len(str(number))
+
+    def reducer(acc, cur):
+        return acc + (cur ** num_digits)
+
+    armstrongified = functools.reduce(reducer, digits, 0)
+    return number == armstrongified
