200c91ccfcab540be9e507a442a3f2edfbb2b90a
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 17:58:06 -0500

    passing

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,16 @@
+import string
+
+
+def rotate_character(character, key):
+    if character not in string.ascii_letters:
+        return character
+
+    alphalist = string.ascii_uppercase if\
+        character.isupper() else\
+        string.ascii_lowercase
+    character_number = (alphalist.index(character) + key) % 26
+    return alphalist[character_number]
+
+
 def rotate(text, key):
-    pass
+    return ''.join([rotate_character(character, key) for character in text])
