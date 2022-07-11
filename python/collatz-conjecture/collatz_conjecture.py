a29b4fa60aa23c8e966beded9993431bd7a5450d
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 18:53:11 -0500

    passing

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,17 @@
+def collatz(number):
+    if number % 2 == 0:
+        number = number / 2
+    else:
+        number = 3 * number + 1
+    return number
+
+
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    steps_taken = 0
+    while number != 1:
+        steps_taken += 1
+        number = collatz(number)
+    return steps_taken
