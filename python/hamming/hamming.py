71cf3365c170a21a6997a8725e3b8fb695b74b43
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 21:36:07 -0500

    passing

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,11 @@
+"""Ham it up"""
+
+
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    ham_distance = 0
+    for index, nuc in enumerate(strand_a):
+        if nuc != strand_b[index]:
+            ham_distance += 1
+    return ham_distance
