9a7d693a8d2c723cc9ffbc7ae9e924e00eaf112d
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 22:21:10 -0500

    passing

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,18 @@
+import math
+
+
+def distance_from_center(x, y):
+    return math.sqrt(x ** 2 + y ** 2)
+
+def distance_to_score(distance):
+    if distance > 10:
+        return 0
+    if distance > 5:
+        return 1
+    if distance > 1:
+        return 5
+    return 10
+
 def score(x, y):
-    pass
+    distance = distance_from_center(x, y)
+    return distance_to_score(distance)
