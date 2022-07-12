1b6a1aa3704c998cb574bc110b58732025619701
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 22:09:05 -0500

    passing

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,2 +1,7 @@
 def transform(legacy_data):
-    pass
+    shiny_data = {}
+    for item in legacy_data.items():
+        score = item[0]
+        for letter in item[1]:
+            shiny_data[letter.lower()] = score
+    return shiny_data
