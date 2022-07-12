d2ad03e0d2113a20dcb1a0a3a479448d14926628
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-07-11 21:46:54 -0500

    passing

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,8 @@
 def to_rna(dna_strand):
-    pass
+    rna = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U",
+    }
+    return "".join(map(lambda nuc: rna[nuc], list(dna_strand)))
